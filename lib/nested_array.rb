9aaaf8d8c01cde81437cf6dddffd687d45f662ed
Chance Mims <chancemims1@gmail.com>
2019-11-28 17:16:32 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -30,8 +30,8 @@ def array_literal_matrix
   # side the ENNET_HOUSE and ENFIELD_TENNIS_ACADEMY Arrays but sorts
   # alphabetically by the first character.
   a_o_a = [
-    ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"],
-    ["Hal Incandenza", "Lyle", "Gerhard Schtitt", "Mario Incandenza", "Michael Pemulis"]
+  ENNET_HOUSE.sort,
+  ENFIELD_TENNIS_ACADEMY.sort
   ]
   a_o_a.sort!
 end
